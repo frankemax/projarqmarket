54d5d80d93187e75ef9b016cea5614c676bd3e74
Max Franke <max.franke@acad.pucrs.br>
2020-05-25 04:48:53 -0300

    FALTA MELHORAR O CSS DA PAGINA DOS AVALIADORES E FAZER OS HANDLE BUTTON

---
+++ src/Client/Score.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import './App.css';
+
+import Dropdown from 'react-dropdown';
+
+
+class Score extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+    }
+
+
+    render() {
+        return (
+            <li>
+
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">{this.props.quesito}</FormLabel>
+                    <RadioGroup row aria-label="position" name="position" defaultValue="1">
+                        <FormControlLabel
+                            value="1"
+                            control={<Radio color="primary"/>}
+                            label="1"
+                            labelPlacement="1"
+                        />
+                        <FormControlLabel
+                            value="2"
+                            control={<Radio color="primary"/>}
+                            label="2"
+                            labelPlacement="1"
+                        />
+                        <FormControlLabel
+                            value="3"
+                            control={<Radio color="primary"/>}
+                            label="3"
+                            labelPlacement="1"
+                        />
+                        <FormControlLabel
+                            value="4"
+                            control={<Radio color="primary"/>}
+                            label="4"
+                            labelPlacement="1"
+                        />
+                        <FormControlLabel value="5" control={<Radio color="primary"/>} label="5"/>
+                        <button className= "submitButton" onClick={this.onSubmit}>Submit</button>
+                    </RadioGroup>
+                </FormControl>
+
+            </li>
+        );
+    }
+
+}
+
+export default Score;
